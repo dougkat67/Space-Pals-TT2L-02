ce77c07f93b42c9de7e56271b2a710289d75fd45
mingfeng5566 <45195067+mingfeng5566@users.noreply.github.com>
2024-05-02 11:11:24 +0800

    ui bar display

--- ui.py
+++ ui.py
@@ -3,7 +3,14 @@ from setting import *
 
 class UI:
     def __init__(self):
-        pass
+        
+        # general
+        self.display_surface = pygame.display.get_surface()
+        self.font = pygame.font.Font(UI_FONT , UI_FONT_SIZE)
+
+        # bar setup
+        self.satiety_bar_rect = pygame.Rect(10,10,FEEDING_BAR_WIDTH, BAR_HEIGHT)
+        self.happy_bar_rect = pygame.Rect(10,34,HAPPINESS_BAR_WIDTH ,BAR_HEIGHT)
     def display(self):
-        pass
+        pygame.draw.rect(self.display_surface, 'black',self.satiety_bar_rect)
         
