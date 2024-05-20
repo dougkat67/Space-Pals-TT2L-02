3c39332e096f43bd710cc9c63c1773ee20c8d63e
isabelle <isabelle1205@gmail.com>
2024-05-20 16:12:04 +0800

    edited clock.py file

--- clock.py
+++ clock.py
@@ -1,11 +1,43 @@
-from time import strftime 
+from tkinter import Tk
+from tkinter import Label
+import time
+import sys
+
+#modified tutorial by adding days
+
+#create window
+master = Tk()
+master.title('Game Clock')
+
+specific_times = ["08:00", "08:15", "08:30", "09:00"]
 
 
-days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+#updates the label
+def update_time_date():                 
+    current_time = time.strftime("%H:%M")
+    current_day = time.strftime("%A")
 
-time_in_day = ["08:00", "12:00", "18:00","20:00", "00:00"]
+    day_label.config(text=current_day)
 
-for day in days_of_week:
-    for time_day in time_in_day:
-     print("Day:", day, "Time in day:", time_day)
-     time.sleep(2)
+
+    if current_time in specific_times:
+        time_label.config(text = current_time)
+    else:
+        time_label.config(text =current_time)
+
+
+    master.after(200, update_time_date)     #200 is every 0.2 of a second, run function again
+
+
+day_label = Label(master, font=('Calibri', 40), bg='grey', fg='white')
+day_label.pack(pady=20)
+
+time_label = Label(master, font=('Calibri', 90), bg='grey', fg='white')
+time_label.pack(pady=20)
+
+update_time_date()
+
+master.mainloop()
+
+
+
